98a4038dbd5a5c002c0789d4c9a5e548b746998e
Rogers <a467j689@wichita.edu>
2020-08-18 14:04:20 -0500

    initial commit

---
+++ Calculator!/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator!");
+            int a = 5;
+                int b = 10;
+                int c = (a * b);
+            Console.WriteLine("When A=5 and B=10, what is A times B?");
+            Console.WriteLine(c);
+        }
+    }
+}
